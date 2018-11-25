14b8bc97434de6e22c09b86467067b491cc58184
Ogive1997 <orautela@macalester.edu>
2018-11-25 10:17:06 -0600

    src_test_copy ray (h, cpp) working

--- src_test copy/Ray.h
+++ src_test copy/Ray.h
@@ -13,19 +13,9 @@ class Ray {
 	Ray (Vect, Vect);
 	
 	// method functions
-	Vect getRayOrigin () { return origin; }
-	Vect getRayDirection () { return direction; }
+	Vect getRayOrigin ();// { return origin; }
+	Vect getRayDirection ();// { return direction; }
 	
 };
-
-Ray::Ray () {
-	origin = Vect(0,0,0);
-	direction = Vect(1,0,0);
-}
-
-Ray::Ray (Vect o, Vect d) {
-	origin = o;
-	direction = d;
-}
 
 #endif
