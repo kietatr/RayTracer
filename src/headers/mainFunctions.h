3974178f514f612be0a25eec6a5d603f017a68cd
Kiet Tran <kietanhtran38@gmail.com>
2018-11-25 12:24:16 -0600

    separate functions in main.cpp to mainFunctions.cpp

---
+++ src/headers/mainFunctions.h
@@ -0,0 +1,42 @@
+#ifndef mainFunctions_h
+#define mainFunctions_h
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <algorithm>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+// new classes created from scratch
+#include "Vect.h"
+#include "Ray.h"
+#include "Camera.h"
+#include "Color.h"
+#include "Light.h"
+
+//classes for objects on screen
+#include "Object.h"
+#include "Source.h"
+#include "Sphere.h"
+#include "Plane.h"
+
+struct RGB {
+	double r;
+	double g;
+	double b;
+};
+
+void saveImage (const char *filename, int w, int h, int dpi, RGB *data);
+int closestObject(vector<double> intersections);
+Color getColorAt(Vect inter_position, Vect inter_ray_direction, vector<Object*> scene_objects, int closest_object, vector<Source*> light_sources, double accuracy, double ambientlight);
+
+#endif
